bbcd1510d5beba3cea2bf401c75dba80bea7f225
dracen <a.hayashi0113@gmail.com>
2019-06-27 00:54:29 +0900

    初期化修正

--- pi_processing/fpv_tank.py
+++ pi_processing/fpv_tank.py
@@ -18,54 +18,18 @@ from pythonosc import dispatcher
 from pythonosc import osc_server
 
 attitude = {}
-servo = {}
-pad = {}
-
 attitude['azimuth'] = 0
 attitude['pitch'] = 0
 attitude['roll'] = 0
 
-done = False
-j = None
-bus = None
+pad = {}
 
-def init_gamepad():
-    global done
-    pygame.init()
-    pygame.joystick.init() # main joystick device system
-    done = True
-
-    pad['0'] = 0
-    pad['1'] = 0
-    pad['2'] = 0
-    pad['3'] = 0
-    pad['4'] = 0
-    pad['5'] = 0
-    pad['6'] = 0
-    pad['7'] = 0
-    pad['8'] = 0
-    pad['9'] = 0
-    pad['x1'] = 0
-    pad['y1'] = 0
-    pad['x2'] = 0
-    pad['y2'] = 0
-
-    try:
-        global j
-        j = pygame.joystick.Joystick(0)  # create a joystick instance
-        j.init() # init instance
-        joyName = j.get_name()
-        print("Enabled joystick: " + joyName)
-    except pygame.error:
-        print("no joystick found.")
+bus = smbus.SMBus(1)
 
 def init_i2c():
     global bus
     bus = smbus.SMBus(1)
-
-def init_control():
-    servo['pan'] = 0
-    servo['tilt'] = 0
+    print("bus restart") 
 
 def r_motor(duty):
     duty = min([max([duty, -100]), 100])
@@ -73,7 +37,6 @@ def r_motor(duty):
     try:
         bus.write_i2c_block_data(0x25, 0x02, [0x01, duty])
     except:
-        print("bus restart") 
         init_i2c()
 
 def l_motor(duty):
@@ -82,16 +45,15 @@ def l_motor(duty):
     try:
         bus.write_i2c_block_data(0x25, 0x02, [0x02, duty])
     except:
-        print("bus restart") 
         init_i2c()
 
 def pan_servo(angle):
+    angle = angle - 40      #現物合わせ
     angle = min([max([angle, -90]), 90])
     angle = angle+90
     try:
         bus.write_i2c_block_data(0x25, 0x01, [0x01, angle, 20])
     except:
-        print("bus restart") 
         init_i2c()
 
 def tilt_servo(angle):
@@ -100,7 +62,6 @@ def tilt_servo(angle):
     try:
         bus.write_i2c_block_data(0x25, 0x01, [0x02, angle, 20])
     except:
-        print("bus restart") 
         init_i2c()
 
 def get_attitude(unused_addr, a, p, r):
@@ -123,8 +84,33 @@ def start_osc_server():
     server.serve_forever()
 
 def gamepad_loop():
-    global j
-    global done
+    pygame.init()
+    pygame.joystick.init() # main joystick device system
+    done = True
+
+    pad['0'] = 0
+    pad['1'] = 0
+    pad['2'] = 0
+    pad['3'] = 0
+    pad['4'] = 0
+    pad['5'] = 0
+    pad['6'] = 0
+    pad['7'] = 0
+    pad['8'] = 0
+    pad['9'] = 0
+    pad['x1'] = 0
+    pad['y1'] = 0
+    pad['x2'] = 0
+    pad['y2'] = 0
+
+    try:
+        j = pygame.joystick.Joystick(0)  # create a joystick instance
+        j.init() # init instance
+        joyName = j.get_name()
+        print("Enabled joystick: " + joyName)
+    except pygame.error:
+        print("no joystick found.")
+
     while done:
         for e in pygame.event.get(): # iterate over event stack
             if e.type == pygame.QUIT:
@@ -138,44 +124,64 @@ def gamepad_loop():
                 pad['y1'] = -int(y1)
                 pad['x2'] = int(x2)
                 pad['y2'] = -int(y2)
-                print(pad)
+                # print(pad)
 
             if e.type == pygame.locals.JOYBUTTONDOWN: # Read the buttons
                 pad[str(e.button)]=1
-                print(pad)
+                # print(pad)
 
             if e.type == pygame.locals.JOYBUTTONUP: # Read the buttons
                 pad[str(e.button)]=0
-                print(pad)  
+                # print(pad)  
 
 
 def control_loop():
+    servo = {}
+    servo['pan'] = 0
+    servo['tilt'] = 0
+
+    motor = {}
+    motor['right'] = 0
+    motor['left'] = 0
+
+    servo_on = False
     while True:
-        l_motor(pad['y1'])
-        r_motor(pad['y2'])
-        
-        if (- 90 <= attitude['azimuth'] <= 90):
+        motor['left'] = pad['y1']
+        motor['right'] = pad['y2']
+
+        if (- 150 <= attitude['azimuth'] <= 150):
             servo['pan'] = -attitude['azimuth']
 
         tmp = -attitude['roll']-90
-        if (- 90 <= tmp <= 90):
+        if (- 150 <= tmp <= 150):
             servo['tilt'] = -tmp
 
-        # print(servo)
-        pan_servo(servo['pan'])
-        tilt_servo(servo['tilt'])
+        if (pad['2'] == 1):
+            servo_on = False
+        if (pad['1'] == 1):
+            servo_on = True
 
+        if(servo_on == True):
+            pan_servo(servo['pan'])
+            tilt_servo(servo['tilt'])
+        else:
+            pan_servo(0)
+            tilt_servo(0)
+            
+        l_motor(motor['left'])
+        r_motor(motor['right'])
+        
+        # print(servo)
+        # print(motor)
         time.sleep(0.1)
 
 init_i2c()
-init_gamepad()
-init_control()
 
 # スレッドに workder1 関数を渡す
 t1 = threading.Thread(target=gamepad_loop)
 t2 = threading.Thread(target=control_loop)
 # スレッドスタート
 t1.start()
+time.sleep(1)
 t2.start()
-
 start_osc_server()
